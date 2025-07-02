bf1b94aa286654e37950a7c83838a383e35bef0c
Timur <timur3424242@gmail.com>
2025-07-02 18:34:44 +0300

    v2
    bag ontscoc

--- ping_pong.py
+++ ping_pong.py
@@ -1,22 +1,25 @@
 from pygame import *
-from random import randint
+import random
 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, size, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.size = size
+        self.image = transform.scale(image.load(player_image), (size))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.speed_x = 10
+        self.speed_y = 0.5
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Racket(GameSprite):
-    def __init__(self,player_image, player_x, player_y, player_speed,key_board):
-        super().__init__(player_image, player_x, player_y, player_speed)
+    def __init__(self,player_image,size, player_x, player_y, player_speed,key_board):
+        super().__init__(player_image, size, player_x, player_y, player_speed)
         self.key_board = key_board
 
     def move(self):
@@ -36,7 +39,8 @@ class Racket(GameSprite):
 
 class Ball(GameSprite):
     def move(self):
-
+        self.rect.x += self.speed_x
+        self.rect.y -= self.speed_y
 
 font.init()
 font1 = font.Font(None, 70)
@@ -47,19 +51,26 @@ display.set_caption('Пинг понг')
 screen_width, screen_height = window.get_size()
 
 background = transform.scale(
-    image.load('assets\image[uEse9L].png'),
+    image.load(r'assets\Untitleddwa.png'),
     (screen_width, screen_height)
 )
 
-ping = Racket('assets\pngimg.com - ping_pong_PNG10369.png', 0, screen_height/2,10,2)
-pong = Racket('assets\pngimg.com - ping_pong_PNG10369.png',screen_width-100, screen_height/2,10,1)
-ball = Ball('assets\pngimg.com - ping_pong_PNG10383.png',screen_width/2,screen_height/2,5)
+x = screen_width/2
+y = screen_height/2
+
+ping = Racket(player_image='assets\pngimg.com - ping_pong_PNG10369.png', size= (100,100), player_x=0, player_y=screen_height/2,player_speed=10,key_board=2)
+pong = Racket(player_image='assets\pngimg.com - ping_pong_PNG10369.png', size= (100,100),player_x=screen_width-100, player_y=screen_height/2,player_speed=10,key_board=1)
+ball = Ball(player_image='assets\pngimg.com - ping_pong_PNG10383.png',size=(50,50),player_x=100, player_y=screen_height/2,player_speed=5)
 
 clock = time.Clock()
 
 FPS = 60
 
+speed_x = 1
+speed_y = 0.1
+
 game = True
+pas = False
 
 while game:
     window.blit(background, (0, 0))
@@ -70,16 +81,33 @@ while game:
     pong.move()
     ball.move()
 
-    ball.rect.x += speed_x
-    ball.rect.y -= speed_y
+    key_pressed = key.get_pressed()
 
-    if ball.rect.y < 0:
-        speed_y *= random.uniform(-1.05, -1)
-    if ball.rect.x > 450 or ball.rect.x < 0:
-        speed_x *= random.uniform(-1.05, -1)
+    if ball.rect.y <= 0:
+        ball.speed_y *= -1
+    if ball.rect.y >= screen_height:
+        ball.speed_y *= -1
 
-    if ball.colliderect(platform.rect):
-        speed_y *= random.uniform(-1.05, -1)
+    if sprite.collide_rect(ball, ping) or sprite.collide_rect(ball, pong):
+        if ball.rect.bottom >= ping.rect.top and ball.rect.bottom <= ping.rect.centery or ball.rect.bottom >= pong.rect.top and ball.rect.bottom <= pong.rect.centery:
+            ball.speed_y = -abs(ball.speed_y)
+        elif ball.rect.top <= ping.rect.bottom and ball.rect.top >= pong.rect.centery or ball.rect.top <= pong.rect.bottom and ball.rect.top >= pong.rect.centery:
+            ball.speed_y = abs(ball.speed_y)
+
+    if key_pressed[K_SPACE] and pas == False:
+        pas = True
+
+    if ball.rect.x > screen_width:
+        a = font1.render(
+            'Игрок 1 победил', True, (255, 255, 255)
+        )
+        window.blit(a, (0, 50))
+
+    if ball.rect.x < 0:
+        b = font1.render(
+            'Игрок 2 победил', True, (255, 255, 255)
+        )
+        window.blit(b, (0, 50))
 
     for e in event.get():
         if e.type == QUIT:
